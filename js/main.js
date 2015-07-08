26061e5f4c7cfec5ac5bcf568b65c933574d7166
noderosa <chie.miyajima@gmail.com>
2015-07-08 19:54:13 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+	$('#submit').keydown(function (enter) {
+		appendFizzBuzz();
+
+	)}
+	
+
+
+
+
+
+var appendFizzBuzz = function (n){
+
+
+ for (var i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            $('result').append("FizzBuzz <br>");
+        }
+        else if (i % 3 === 0) {
+            $('result').append("Fizz <br>");
+        }
+        else if (i % 5 === 0) {
+            $('result').append("Buzz <br>");
+        }
+        else {
+            $('result').append(i+"<br>");
+        }
+
+
+  }
+}
+
+});
+
+
+
+
+
