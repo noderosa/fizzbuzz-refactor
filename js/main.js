8fe5e89b3832903817375eda129b93b426bc5f7a
noderosa <chie.miyajima@gmail.com>
2015-07-08 21:09:37 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 $(document).ready(function () {
 
 	$('#submit').on('click', function (enter) {
-		appendFizzBuzz();
+		enter.preventDefault();
+		var num = $('#userinput').val();
+		if (num == "" || isNaN(num)){
+			alert("Please enter a number.");
+		}
+		
+		else if (num % 1 != 0){
+			alert("Please enter a whole number only.");
+		}
 
-	)}
+		else if (num >= 100 || num <= 0){
+			alert("Please enter a number between 1 and 100.");
+		}
+
+		else {
+
+			appendFizzBuzz(num);
+		}
+	});
 	
 
 
 
-
-
 var appendFizzBuzz = function (n){
-
+	$('result').html("");
 
  for (var i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
